6079e275414c8fd6796965366ee561114f86e06a
Florence Hapgood <flohapgood@icloud.com>
2018-10-19 17:26:14 +0100

    step 9 with errors

--- note-controller.js
+++ note-controller.js
@@ -10,14 +10,34 @@
 
     var element = document.getElementById('app');
     element.innerHTML = this.noteListView.returnListHTML()
-
-
   };
+
+   // NoteController.prototype.makeUrlChangeShowNoteForCurrentPage = function () {
+   //      window.addEventListener("hashchange", showNoteForcurrentPage());
+   //    };
+   //
+   //   NoteController.prototype.showNoteForcurrentPage = function() {
+   //      showNote(getNoteIdFromUrl(window.location));
+   //    };
+   //
+   //  NoteController.prototype.getNoteIdFromUrl = function(location) {
+   //      return location.hash.split("#")[1]; //we're taking the noteid from the url so we know which note to get
+   //    };
+   //
+   //  NoteController.prototype.showNote = function(noteId) {
+   //
+   //      var note = this.notelist.list[noteId].text
+   //
+   //      document
+   //        .getElementById("noteText")
+   //        .innerHTML = note;
+   //    };
 
 
  exports.NoteController = NoteController;
 })(this);
 
 
-//var test = new NoteController()
-//test.uploadList()
+//
+// var noteController = new NoteController()
+// noteController.makeUrlChangeShowNoteForCurrentPage()
